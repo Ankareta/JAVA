31ddbeccfb1a7e5ff6b29c41d84a2d5e2c39d1fb
ankareta <carolinankaret@gmail.com>
2023-09-26 19:25:22 -0600

    Esta carpeta empezamos a usar JAVA, conocimos los valores min y max de los tipos de datos

---
+++ chicharron.java
@@ -0,0 +1,10 @@
+import java.util.*
+
+public class chicharron {
+
+public static void main(String[] args) throws Exception {
+        // Your code here!
+        
+        System.out.println("XXXXXXXX");
+    }
+}
